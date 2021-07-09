1101e8ec7734186c4d93872a2edbaa611088ab20
maxson.carvalho <maxson.carvalho@findes.org.br>
2021-07-09 17:53:15 -0300

    Commit 1

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function consultaCep(){
+    var cep = document.getElementById("cep").value;
+    var url = `https://viacep.com.br/ws/${cep}/json/`
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function(response){
+            console.log(response);
+            //alert(response.logradouro);
+            //document.getElementById("logradouro").innerHTML = response.logradouro;
+            $("#logradouro").html(response.logradouro);            
+            //document.getElementById("bairro").innerHTML = response.bairro;
+            $("#bairro").html(response.bairro);
+            document.getElementById("localidade").innerHTML = response.localidade;
+            document.getElementById("uf").innerHTML = response.uf;
+        }
+    })
+}
